f798d0a2a5a913876f79f7c85b0d5ab141c60e19
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-10-12 23:50:45 +0400

    update

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+       <>
+        <Nav>
+            <NavContainer>
+                <NavLogo>
+                    Dolla
+                </NavLogo>
+            </NavContainer>
+        </Nav>
+       </>
+    )
+}
+
+export default Navbar
